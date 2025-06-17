308b9db5e6b806c669aece9bf3185a13b2c2cfeb
Aman <amandeepcodes@gmail.com>
2025-06-17 15:58:43 -0600

    Added dependencies to network module

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -40,4 +41,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.retrofit)
+    implementation(libs.gson.converter)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
 }
